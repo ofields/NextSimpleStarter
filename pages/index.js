2fc0f2e2c15f1ad41a08003c18f5924ae15ae358
ofields <ofields@gmail.com>
2020-03-30 17:27:37 +0000

    buttons

--- pages/index.js
+++ pages/index.js
@@ -2,20 +2,91 @@ import 'isomorphic-fetch'
 import React from 'react'
 import Fork from '../components/Fork'
 import Todo from '../components/Todo'
+import Button from '@material-ui/core/Button'
 
 const Index = ({ stars }) => (
 	<React.Fragment>
-		<Fork stars={stars} />
-		<Todo />
+		{/* <Fork stars={stars} /> */}
+		{/* <Todo /> */}
+
+	<Button variant="contained" color="primary">Primary</Button>
+	<Button variant="contained" color="primary">Primary</Button>
+	<Button variant="contained" color="primary">Primary</Button>
+	<Button variant="contained" color="primary">Primary</Button>
+	<Button variant="contained" color="primary">Primary</Button>
+	<Button variant="contained" color="primary">Primary</Button>
+	<Button variant="contained" color="primary">Primary</Button>
+	<Button variant="contained" color="primary">Primary</Button>
+	<Button variant="contained" color="primary">Primary</Button>
+	<Button variant="contained" color="primary">Primary</Button>
+	<Button variant="contained" color="primary">Primary</Button>
+	<Button variant="contained" color="primary">Primary</Button>
+	<Button variant="contained" color="primary">Primary</Button>
+	<Button variant="contained" color="primary">Primary</Button>
+	<Button variant="contained" color="primary">Primary</Button>
+	<Button variant="contained" color="primary">Primary</Button>
+	<Button variant="contained" color="primary">Primary</Button>
+	<Button variant="contained" color="primary">Primary</Button>
+	<Button variant="contained" color="primary">Primary</Button>
+	<Button variant="contained" color="primary">Primary</Button>
+	<Button variant="contained" color="primary">Primary</Button>
+	<Button variant="contained" color="primary">Primary</Button>
+	<Button variant="contained" color="primary">Primary</Button>
+	<Button variant="contained" color="primary">Primary</Button>
+	<Button variant="contained" color="primary">Primary</Button>
+	<Button variant="contained" color="primary">Primary</Button>
+	<Button variant="contained" color="primary">Primary</Button>
+	<Button variant="contained" color="primary">Primary</Button>
+	<Button variant="contained" color="primary">Primary</Button>
+	<Button variant="contained" color="primary">Primary</Button>
+	<Button variant="contained" color="primary">Primary</Button>
+	<Button variant="contained" color="primary">Primary</Button>
+	<Button variant="contained" color="primary">Primary</Button>
+	<Button variant="contained" color="primary">Primary</Button>
+	<Button variant="contained" color="primary">Primary</Button>
+	<Button variant="contained" color="primary">Primary</Button>
+	<Button variant="contained" color="primary">Primary</Button>
+	<Button variant="contained" color="primary">Primary</Button>
+	<Button variant="contained" color="primary">Primary</Button>
+	<Button variant="contained" color="primary">Primary</Button>
+	<Button variant="contained" color="primary">Primary</Button>
+	<Button variant="contained" color="primary">Primary</Button>
+	<Button variant="contained" color="primary">Primary</Button>
+	<Button variant="contained" color="primary">Primary</Button>
+	<Button variant="contained" color="primary">Primary</Button>
+	<Button variant="contained" color="primary">Primary</Button>
+	<Button variant="contained" color="primary">Primary</Button>
+	<Button variant="contained" color="primary">Primary</Button>
+	<Button variant="contained" color="primary">Primary</Button>
+	<Button variant="contained" color="primary">Primary</Button>
+	<Button variant="contained" color="primary">Primary</Button>
+	<Button variant="contained" color="primary">Primary</Button>
+	<Button variant="contained" color="primary">Primary</Button>
+	<Button variant="contained" color="primary">Primary</Button>
+	<Button variant="contained" color="primary">Primary</Button>
+	<Button variant="contained" color="primary">Primary</Button>
+	<Button variant="contained" color="primary">Primary</Button>
+	<Button variant="contained" color="primary">Primary</Button>
+	<Button variant="contained" color="primary">Primary</Button>
+	<Button variant="contained" color="primary">Primary</Button>
+	<Button variant="contained" color="primary">Primary</Button>
+	<Button variant="contained" color="primary">Primary</Button>
+	<Button variant="contained" color="primary">Primary</Button>
+	<Button variant="contained" color="primary">Primary</Button>
+	<Button variant="contained" color="primary">Primary</Button>
+	<Button variant="contained" color="primary">Primary</Button>
+	<Button variant="contained" color="primary">Primary</Button>
+	<Button variant="contained" color="primary">Primary</Button>
+
 	</React.Fragment>
 )
 
-Index.getInitialProps = async () => {
-	const res = await fetch(
-		'https://api.github.com/repos/ooade/NextSimpleStarter'
-	)
-	const json = await res.json()
-	return { stars: json.stargazers_count }
-}
+// Index.getInitialProps = async () => {
+// 	const res = await fetch(
+// 		'https://api.github.com/repos/ooade/NextSimpleStarter'
+// 	)
+// 	const json = await res.json()
+// 	return { stars: json.stargazers_count }
+// }
 
 export default Index
